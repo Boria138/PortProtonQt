9c0422c5c80f0cd27de00771c577bc67b941806e
Boris Yumankulov <boria138@altlinux.org>
2025-04-24 20:30:46 +0500

    Load QT bundled translations

--- portprotonqt/app.py
+++ portprotonqt/app.py
@@ -1,16 +1,30 @@
 import sys
-from portprotonqt.main_window import MainWindow
+from PySide6.QtCore import QLocale, QTranslator, QLibraryInfo
 from PySide6.QtWidgets import QApplication
+from portprotonqt.main_window import MainWindow
 from portprotonqt.tray import SystemTray
 from portprotonqt.config_utils import read_theme_from_config
+from portprotonqt.logger import get_logger
+
+logger = get_logger(__name__)
+
 def main():
     app = QApplication(sys.argv)
+    system_locale = QLocale.system()
+    qt_translator = QTranslator()
+    translations_path = QLibraryInfo.path(QLibraryInfo.LibraryPath.TranslationsPath)
+    if qt_translator.load(system_locale, "qtbase", "_", translations_path):
+        app.installTranslator(qt_translator)
+    else:
+        logger.error(f"Qt translations for {system_locale.name()} not found in {translations_path}")
+
     window = MainWindow()
     current_theme_name = read_theme_from_config()
     tray = SystemTray(app, current_theme_name)
     tray.show_action.triggered.connect(window.show)
     tray.hide_action.triggered.connect(window.hide)
     window.show()
+
     sys.exit(app.exec())
 
 if __name__ == '__main__':
