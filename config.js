1a9228b76d69b3abb4c152c801df472d99a564e1
Boris Yumankulov <boria138@altlinux.org>
2025-06-20 21:37:14 +0500

    ci: added renovate auto update bot
    Signed-off-by: Boris Yumankulov <boria138@altlinux.org>

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "endpoint": "https://git.linux-gaming.ru/api/v1",
+    "gitAuthor": "Renovate Bot <noreply@linux-gaming.ru>",
+    "platform": "gitea",
+    "onboardingConfigFileName": "renovate.json",
+    "autodiscover": true,
+    "optimizeForDisabled": true,
+};
