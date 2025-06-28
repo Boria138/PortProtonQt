c862a39225b5651047fa3b34b36491487d46b7ff
Boris Yumankulov <boria138@altlinux.org>
2025-06-28 09:42:22 +0500

    feat(renovate): use config from Linux-Gaming repo
    Signed-off-by: Boris Yumankulov <boria138@altlinux.org>

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "endpoint": "https://git.linux-gaming.ru/api/v1",
-    "gitAuthor": "Renovate Bot <noreply@linux-gaming.ru>",
-    "platform": "gitea",
-    "onboardingConfigFileName": "renovate.json",
-    "autodiscover": true,
-    "optimizeForDisabled": true,
-};
