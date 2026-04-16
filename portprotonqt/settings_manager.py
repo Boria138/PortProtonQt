4321d215eabd9a4d99bc3992a3ff94c49147783a
Boris Yumankulov <boria138@altlinux.org>
2026-04-16 18:29:55 +0500

    chore(settings): drop PW_USE_SAREK_ASYNC
    Signed-off-by: Boris Yumankulov <boria138@altlinux.org>

--- portprotonqt/settings_manager.py
+++ portprotonqt/settings_manager.py
@@ -53,7 +53,6 @@ def get_toggle_settings():
         'PW_USE_WINED3D_VULKAN': _("Use WineD3D Vulkan backend (Damavand)"),
         # Proton
         'PW_USE_SUPPLIED_DXVK_VKD3D': _("Use bundled dxvk/vkd3d from Wine/Proton"),
-        'PW_USE_SAREK_ASYNC': _("Use async dxvk-sarek (experimental)"),
         # Power management
         'PW_USE_INHIBIT_SLEEP_INFO': _("Prevent the system from going to sleep and disable the screensaver while the game is running"),
         # Runtime
