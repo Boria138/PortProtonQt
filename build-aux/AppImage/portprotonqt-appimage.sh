0cd98c0996957e02bcbdec453daec700c0740a95
Boris Yumankulov <boria138@altlinux.org>
2026-07-13 22:24:45 +0500

    fix(appimage): skip portwine_db strace
    Signed-off-by: Boris Yumankulov <boria138@altlinux.org>

--- build-aux/AppImage/portprotonqt-appimage.sh
+++ build-aux/AppImage/portprotonqt-appimage.sh
@@ -27,6 +27,16 @@ chmod +x ./quick-sharun
 mkdir -p ./AppDir/etc/udev/rules.d
 cp /usr/lib/udev/rules.d/60-portprotonqt.rules ./AppDir/etc/udev/rules.d
 
+# Add PortProton scripts
+# Copy manual because im to lazzy for wait ./quick-sharun strace 208 .ppdb
+mkdir -p ./AppDir/share
+if [ -d /usr/local/share/portproton ]; then
+       cp -r /usr/local/share/portproton ./AppDir/share
+elif [ -d /usr/share/portproton ]; then
+       cp -r /usr/share/portproton ./AppDir/share
+fi
+
+
 # Deploy dependencies
 # Qt libs have to be passed manually due to the app being a python script
 ./quick-sharun \
@@ -35,7 +45,6 @@ cp /usr/lib/udev/rules.d/60-portprotonqt.rules ./AppDir/etc/udev/rules.d
 	/usr/lib/libQt6Core.so* \
 	/usr/lib/libQt6Gui.so* \
 	/usr/lib/libQt6Network.so* \
-	/usr/share/portproton \
 	/usr/lib/qt6/plugins/imageformats/libqwebp.so
 
 # Turn AppDir into AppImage
